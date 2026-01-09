38e47d416a86ef5207178c33f7f3fb418c98c2d3
valjeanjean <ilian.metina83@gmail.com>
2026-01-09 15:23:35 +0100

    adding video posters

--- app/page.tsx
+++ app/page.tsx
@@ -15,35 +15,40 @@ import { VideoCarousel } from '@/components/sections/video-carousel'
 const videos = [
   {
     id: '1',
-    videoPath: '/videos/video1.mp4', // ✅ Changé 'src' en 'videoPath'
+    videoPath: '/videos/video1.mp4',
+    poster: '/videos/video1-poster.jpg',
     title: 'Vidéo 1',
     videoId: 'video1',
     subtitle: 'Sous-titre 1',
   },
   {
     id: '2',
-    videoPath: '/videos/video2.mp4', // ✅ Changé 'src' en 'videoPath'
+    videoPath: '/videos/video2.mp4',
+    poster: '/videos/video2-poster.jpg',
     title: 'Vidéo 2',
     videoId: 'video2',
     subtitle: 'Sous-titre 2',
   },
   {
     id: '3',
-    videoPath: '/videos/video3.mp4', // ✅ Changé 'src' en 'videoPath'
+    videoPath: '/videos/video3.mp4',
+    poster: '/videos/video3-poster.jpg',
     title: 'Vidéo 3',
     videoId: 'video3',
     subtitle: 'Sous-titre 3',
   },
   {
     id: '4',
-    videoPath: '/videos/video4.mp4', // ✅ Changé 'src' en 'videoPath'
+    videoPath: '/videos/video4.mp4',
+    poster: '/videos/video4-poster.jpg',
     title: 'Vidéo 4',
     videoId: 'video4',
     subtitle: 'Sous-titre 4',
   },
   {
     id: '5',
-    videoPath: '/videos/video5.mp4', // ✅ Changé 'src' en 'videoPath'
+    videoPath: '/videos/video5.mp4',
+    poster: '/videos/video5-poster.jpg',
     title: 'Vidéo 5',
     videoId: 'video5',
     subtitle: 'Sous-titre 5',
