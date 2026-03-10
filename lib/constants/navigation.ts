25a0f69f136e5504226c3d90d1ae21cc7dcec2f6
Ilian Metina <ilian.metina83@gmail.com>
2026-03-10 11:34:10 +0100

    fix: type menuItems to allow optional target property

--- lib/constants/navigation.ts
+++ lib/constants/navigation.ts
@@ -1,4 +1,4 @@
-export const menuItems = [
+export const menuItems: { name: string; href: string; target?: string }[] = [
   { name: 'Réalisations', href: '/#realisations' },
   { name: 'Témoignages', href: '/#testimonials-video' },
   // { name: 'Tarifs', href: '/#pricing' },
